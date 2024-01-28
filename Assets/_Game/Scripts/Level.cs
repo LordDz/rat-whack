b4f7bc872eab3f271fe9871f6a8f8a12955f6f8e
David.Zetterdahl <david.zetterdal@geelydesign.com>
2024-01-28 16:41:53 +0100

    Level music

---
+++ Assets/_Game/Scripts/Level.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets._Game.Scripts
+{
+    public class Level : MonoBehaviour
+    {
+        public int levelIndex = 0;
+        float timeDivider = 0.1f;
+        [SerializeField] AudioSource levelMusic;
+
+        private bool levelStarted = false;
+
+        private void Start()
+        {
+            LevelStart();
+        }
+
+        private void Update()
+        {
+            if (levelStarted && !levelMusic.isPlaying)
+            {
+                LevelDone();
+            }
+        }
+
+        private void LevelStart()
+        {
+            levelMusic.Play();
+            if (levelIndex > 0)
+            {
+                Time.timeScale = 1 + (levelIndex / timeDivider);
+            }
+            Debug.Log("Level Started with timescale: " + Time.timeScale);
+
+            levelIndex++;
+            levelStarted = true;
+        }
+
+        private void LevelDone()
+        {
+            Debug.Log("Level done");
+            LevelStart();
+        }
+    }
+}
